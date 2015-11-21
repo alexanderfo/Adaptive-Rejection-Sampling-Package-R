333b605183f674a7aeb6851b843552a821cc0e14
jilwang <jilwang@berkeley.edu>
2015-11-21 14:33:01 -0800

    initial task assignment

---
+++ modular functions.R
@@ -0,0 +1,44 @@
+# NAMING CONVENTION!
+
+# noun for variables
+# verb for functions
+# underscore to link words
+# lowercase for everything
+
+matrix of vertices and derivatives(slopes)
+
+create_segment <- function(v1, v2){
+
+}
+
+
+# Ji and Alex
+update_u <- fucntion(vertices, slopes){
+	pass
+}
+
+update_l <- fucntion(vertices){
+	pass
+}
+
+update_s <- fucntion(vertices, slopes){
+	pass
+}
+
+# Mengfei & Daisy
+draw_sample(s, m = 1){
+	pass
+}
+
+
+insert_new_vertex(vertices, new_vertex){
+	pass
+}
+
+evaluate_deriv(h){
+	pass
+}
+
+create_z(u){
+	return(z)
+}
