8d3c917f6258047176433f5fff575266888aef3f
Mengfei Jiang <mengfei.jiang@berkeley.edu>
2015-11-21 23:52:07 -0800

    draw_sample function first draft in non-vectorized way

--- modular functions.R
+++ modular functions.R
@@ -21,14 +21,45 @@ update_l <- fucntion(vertices){
 	pass
 }
 
-update_s <- fucntion(vertices, slopes){
-	pass
+# Mengfei & Daisy
+draw_sample <- function(envelope, z, num_of_samples = 1){
+	# Draw sampling point candidates
+  # Args: 
+  #   envelope: vector of exponentiated linear envelope functions
+  #   z: vector of intersection points
+  # Output:
+  #   candidates: vector sampling candidates
+  numerator <- sapply(1:length(envelope), function(i) integrate(envelope[[i]], z[i], z[i+1])$value)
+  denominator <- sum(numerator)
+  cdf_end_pts <- cumsum(numerator/denominator)
+  w <- runif(num_of_samples)
+  # locate the bin each w falls in, i.e. find i
+  w_idx <- sapply(w, function(i) which(i <= cdf_end_pts)[1])
+  f <- function(x, i, w_i) {
+    # Args:
+    #   x: one sampling candidate
+    #   i: the index of the Uniform variable in the w vector
+    #   w_i: the bin index that the Uniform variable falls in
+    integrate(envelope[[w_i]], lower = z[w_i], upper = x)$value/denominator - w[i] + ifelse(w_i == 1, 0, cdf_end_pts[w_i-1])
+    }
+  candidates <- sapply(1:num_of_samples, 
+                       function(i) {
+                         uniroot(f,interval = c(z[w_idx[i]], z[w_idx[i]+1]), 
+                                 i, w_idx[i], tol = 1e-6)$root})
+  return(candidates)
 }
 
-# Mengfei & Daisy
-draw_sample(s, m = 1){
-	pass
-}
+# little test
+f1 <- function(x) exp(x)
+f2 <- function(x) exp(-(x-1))
+envelope <- c(f1, f2)
+z <- c(-1, 0.5, 2)
+# see code efficiency
+tmp <- tempfile()
+Rprof(tmp, interval = 0.1)
+a=draw_sample(envelope, z, num_of_samples = 100000)
+Rprof(NULL)
+summaryRprof(tmp)
 
 
 insert_new_vertex(vertices, new_vertex){
