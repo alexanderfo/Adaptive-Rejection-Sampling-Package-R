82867886019c83eafff37ead6137354562145bab
jilwang <jilwang@berkeley.edu>
2015-12-10 16:51:44 -0800

    rerun tests - passed a lot

--- ars/tests/test-main.R
+++ ars/tests/test-main.R
@@ -13,7 +13,7 @@ compare_densities<-function(x_real,x_ars) {
 }
 
 
-n<-500 #number of samples
+n<-100 #number of samples
 
 
 ##### Truncated normal
@@ -24,35 +24,29 @@ compare_densities(x_real,x_ars) #OK
 
 
 
-##### Exponential dist, PARTIALLY DOES NOT WORK
+##### Exponential dist
+#problem with the limits can't choose upper>800, lower<0 (where the distribution is 0 only numerically)
 x_real <- rexp(n)
-x_ars<-arSampler(dexp,n,0.000001,700) #problem with the limits can't choose upper>800, lower=0
-#Problem in draw_sample with the integrate function
-
-compare_densities(x_real,x_ars)
+x_ars<-arSampler(dexp,n,0,700) 
+compare_densities(x_real,x_ars) #OK
 
 
 
-##### Uniform dist, DOES NOT WORK
-x_real <- runif(n)
-x_ars <- arSampler(dunif,n,0.5,0.6)
-#Problem in draw_sample with the integrate function
+##### Uniform dist
+x_real <- runif(n,-5,5)
+x_ars <- arSampler(dunif,n,-5,5, -5, 5)
+compare_densities(x_real,x_ars) #OK
 
 
-###### Laplace distribution (double exponential), DOES NOT WORK 
-
-# in draw sample integrate does not work: numerator <- sapply(1:length(envelope), function(i) integrate(envelope[[i]], z[i], z[i+1])$value)
-# Produces two errors: Error in integrate(envelope[[i]], z[i], z[i + 1]) : a limit is missing (NaN is created)
-# and Error in integrate(envelope[[i]], z[i], z[i + 1]) : non-finite function value
+###### Laplace distribution (double exponential)
 library(smoothmest)
 
 laplace_pdf <- function(x) ddoublex(x, mu=0, lambda=1)
 
 x_real <- rdoublex(n,mu=0,lambda=1)
-x_ars <- arSampler(laplace_pdf,n,-1,1) #does not work with a greater interval if we include zero in the interval
-# I changed in draw_sample, then it worked for some intervals, very unstable
+x_ars <- arSampler(laplace_pdf,n,-10,10)
 
-compare_densities(x_real,x_ars)
+compare_densities(x_real,x_ars) #OK
 
 
 # Test Weibull density
@@ -60,41 +54,35 @@ compare_densities(x_real,x_ars)
 weibull_pdf <- function(x) dweibull(x,shape=1)
 
 x_real <- rweibull(n,shape=1)
-x_ars <- arSampler(weibull_pdf,n,0.0001,700) #does not work for lower=0 and upper>800
+x_ars <- arSampler(weibull_pdf,n,0,700) #does not work for lower=0 and upper>800
 
 
 compare_densities(x_real,x_ars) #OK
 
 
-# Test chi-square density DOES NOT WORK
+# Test chi-square density
 
 chisq_pdf <- function(x) dchisq(x,1)
 x_ars <- arSampler(chisq_pdf,n,0.00001,100) # is not log-concave for parameter, degrees of freedom = 1.
-is_logconcave(chisq_pdf,0.00001,100)
 
-#Ok
-chisq_pdf <- function(x) dchisq(x,2)
-is_logconcave(chisq_pdf,0.00001,100)
+
 #Ok logconcave when df>=2
+chisq_pdf <- function(x) dchisq(x,3)
 
-x_real <- rchisq(n,2)
-x_ars <- arSampler(chisq_pdf,n,0.00001,100)
+x_real <- rchisq(n,3)
+x_ars <- arSampler(chisq_pdf,n,0,100)
 
 compare_densities(x_real,x_ars) #OK
 
 
-
-
-
-
 # Maybe we can use the trapezoidal rule to integrate instead?
-trapezoid <- function(fun, a, b, n=100) {
-  # numerical integral of fun from a to b
-  # using the trapezoid rule with n subdivisions
-  # assume a < b and n is a positive integer
-  h <- (b-a)/n
-  x <- seq(a, b, by=h)
-  y <- fun(x)
-  s <- h * (y[1]/2 + sum(y[2:n]) + y[n+1]/2)
-  return(s)
-}
+# trapezoid <- function(fun, a, b, n=100) {
+#   # numerical integral of fun from a to b
+#   # using the trapezoid rule with n subdivisions
+#   # assume a < b and n is a positive integer
+#   h <- (b-a)/n
+#   x <- seq(a, b, by=h)
+#   y <- fun(x)
+#   s <- h * (y[1]/2 + sum(y[2:n]) + y[n+1]/2)
+#   return(s)
+# }
